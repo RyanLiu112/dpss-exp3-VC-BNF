2a9dd7eedbf5762db5163cee18efc55836338722
RyanLiu <1036433215@qq.com>
2024-01-06 21:40:21 +0800

    save version3

--- plot.py
+++ plot.py
@@ -5,13 +5,13 @@ model_type = 'one'
 # model_type = 'one_res'
 # model_type = 'many'
 
-train = np.load(f'./{model_type}_train_loss.npy')
-valid = np.load(f'./{model_type}_valid_loss.npy')
+train = np.load(f'./{model_type}_train_loss.npy', allow_pickle=True)
+valid = np.load(f'./{model_type}_valid_loss.npy', allow_pickle=True)
 
 plt.figure(figsize=(6, 4))
-plt.plot(train, label='train')
-plt.xlabel('epoch')
-plt.ylabel('loss')
+plt.plot(train)
+plt.xlabel('Step')
+plt.ylabel('Loss')
 plt.legend()
 plt.tight_layout()
 plt.savefig(f'./{model_type}_train_loss.png')
